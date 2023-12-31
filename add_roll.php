8c862e55d4abdc8a55b9425c0049f70da9a31c03
amirholakoo <amirholakoo@gmail.com>
2023-12-31 10:25:31 -0800

    Update add_roll.php

--- add_roll.php
+++ add_roll.php
@@ -1,5 +1,8 @@
+
 <?php
 include 'connect_db.php';
+include 'phpqrcode/qrlib.php';
+
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -11,17 +14,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $length = $_POST['length'];
     $breaks = $_POST['breaks'];
     $comments = $_POST['comments'];
+$qrData = "Roll Number: $reel_number, GSM: $gsm, Grade: $grade, Width: $width, Length: $length";
 
-    $sql = "INSERT INTO rolls (reel_number, gsm, grade, width, length, breaks, comments)
-    VALUES ('$reel_number', '$gsm', '$grade', '$width', '$length', '$breaks', '$comments')";
+    $sql = "INSERT INTO rolls (reel_number, gsm, grade, width, length, breaks, comments, qr_code)
+    VALUES ('$reel_number', '$gsm', '$grade', '$width', '$length', '$breaks', '$comments', '$qrData')";
+if ($conn->query($sql) === TRUE) {
+    echo "New roll added successfully<br>";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "New roll added successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    // Generate QR code
+    $qrCodeFile = 'qrcodes/roll_' . $reel_number . '.png';
+
+
+    // Check if qrcodes directory exists, if not create it
+    if (!file_exists('qrcodes')) {
+        mkdir('qrcodes', 0777, true);
     }
+
+    // Generate and save the QR code image
+    QRcode::png($qrData, $qrCodeFile);
+
+    echo "QR Code generated!";
+echo "<br><br><a href='$qrCodeFile'>Download QR Code</a>";
+echo "<br><img src='$qrCodeFile' />";
+
+echo "<br><br><a href='rolls.html'>Enter Another Roll</a>";
+        echo "<br><a href='index.html'>Return to Dashboard</a>";
+
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 
     // Close database connection
     closeDbConnection($conn);
 }
 ?>
+
+
